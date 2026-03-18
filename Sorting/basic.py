7c87fb5c421fe407dd055109c604fc5ada6fee11
milksnake88 <110155171+milksnake88@users.noreply.github.com>
2026-03-18 09:10:49 +0000

    [feat] 문자열을 구분자로 나누어 리스트로 반환하는 split() 메서드 예제 추가

---
+++ Sorting/basic.py
@@ -0,0 +1,7 @@
+# split()
+# split()은 문자열을 특정 구분자로 나누어 리스트로 반환하는 메서드
+# 기본적으로 공백을 구분자로 사용하지만, 다른 구분자를 지정할 수도 있음
+input_str = input() # milk snake 88
+input_split_list = input().split() # milk snake 88
+print(input_str) # milk snake 88
+print(input_split_list) # >> ['milk', 'snake', '88']
