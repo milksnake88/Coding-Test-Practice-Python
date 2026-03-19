85f3a3299c5bd167596986099ba61f40e164162b
milksnake88 <110155171+milksnake88@users.noreply.github.com>
2026-03-19 09:30:00 +0000

    [docs] list(), map() 함수 설명 추가

--- basic.py
+++ basic.py
@@ -5,3 +5,15 @@ input_str = input() # milk snake 88
 input_split_list = input().split() # milk snake 88
 print(input_str) # milk snake 88
 print(input_split_list) # >> ['milk', 'snake', '88']
+
+# list()
+# list()는 반복 가능한 객체(문자열, 튜플, 집합 등)를 받아
+# 새로운 리스트 객체를 생성하거나 빈 리스트를 만드는 내장 함수
+print(list("hello")) # >> ['h', 'e', 'l', 'l', 'o']
+print(list((1, 2, 3))) # >> [1, 2, 3]
+
+# map()
+# map()은 각 요소에 함수를 자동으로 반복 적용해주는 함수
+# 실제 값이 아니라 iterator
+# map(int, "123")일 경우 내부적으로 [int('1'), int('2'), int('3')]
+# list(map(int, "123")) => [1, 2, 3]
