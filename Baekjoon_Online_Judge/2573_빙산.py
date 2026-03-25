c9754b6a7e119e736e287a7489f900ff9ed7d829
milksnake88 <110155171+milksnake88@users.noreply.github.com>
2026-03-25 12:36:16 +0000

    [update] 빙산 문제 문어선생님 버전

--- Baekjoon_Online_Judge/2573_빙산.py
+++ Baekjoon_Online_Judge/2573_빙산.py
@@ -1,10 +1,17 @@
 # https://www.acmicpc.net/problem/2573
 
+# for year (1, 900000):
+# [1] 둘러싼 0의 개수 카운트 sub array
+# [2] 높이 낮추기: arr[i][j] = max(0, arr[i][j]-sub[i][j])
+# [3] 덩어리 개수 카운트 (BFS)
+#     cnt > 1 인 경우 -> return year
+#     cnt == 0 인 경우 -> return 0
+
 ############################################################
 
 from collections import deque
 
-def bfs(si, sj):
+def bfs(si, sj, v):
     q = deque()
 
     q.append((si, sj))
@@ -18,39 +25,48 @@ def bfs(si, sj):
                 q.append((ni, nj))
                 v[ni][nj] = 1
             
-def solve():
-    global v
-    cnt_years = 0
-    while True:
-        cnt_zero = 0
-        cnt_bfs = 0
-        ice = [[0]*m for _ in range(n)]
-        for ci in range(n):
-            for cj in range(m):
-                if arr[ci][cj] <= 0:
-                    cnt_zero += 1
-                else:
-                    ice[ci][cj] = 1
+def solve(): # 1~900000 년, 전체 순회 반복 작업
+    for year in range(1, 900000):
+        # [1] 네방향 0의 개수 카운트
+        a_sub = [[0]*m for _ in range(n)]
+        for i in range(1, n-1): # 가장자리는 방산 x
+            for j in range(1, m-1):
+                if arr[i][j]==0:
+                    continue
                 for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
-                    ni, nj = ci+di, cj+dj
-                    if 0<=ni<n and 0<=nj<m and arr[ni][nj] <= 0 and not ice[ni][nj]:
-                        arr[ci][cj] -= 1
-        if cnt_zero == n*m:
-            return 0
+                    ni, nj = i+di, j+dj
+                    if arr[ni][nj] == 0:
+                        a_sub[i][j] += 1
+        
+        # [2] 높이 낮추기
+        # 한 루프보다 나눠서 짜는게 디버깅하기 편리하고 실수를 줄일 수 있음
+        for i in range(1, n-1):
+            for j in range(1, m-1):
+                if a_sub[i][j] > 0:
+                    arr[i][j] = max(0, arr[i][j]-a_sub[i][j])
+        
+        # [3] 빙산 덩어리 개수 카운트
         v = [[0]*m for _ in range(n)]
-        for ci in range(n):
-            for cj in range(m):
+        cnt = 0
+        for ci in range(n-1):
+            for cj in range(m-1):
                 if arr[ci][cj] > 0 and not v[ci][cj]:
-                    bfs(ci, cj)
-                    cnt_bfs += 1
-        cnt_years += 1
-        if cnt_bfs >= 2:
-            return cnt_years
+                    bfs(ci, cj, v)
+                    cnt += 1
+                    if cnt > 1: # 두 덩어리 이상... for문 안에다 체크해서 시간 아끼기
+                        return year
+        if cnt==0:
+            return 0
+    
+    # 이 자리에 올 일은 없지만...
+    return -1
+
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-        
-print(solve())
+
+ans = solve()
+print(ans)
 
 
 
