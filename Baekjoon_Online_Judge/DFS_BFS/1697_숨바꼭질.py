1e2271a2b769523102d7347f440e0c9133ffcfd5
milksnake88 <110155171+milksnake88@users.noreply.github.com>
2026-03-26 01:56:42 +0000

    [feat] 숨바꼭질 문제 추가

--- Baekjoon_Online_Judge/DFS_BFS/1697_숨바꼭질.py
+++ Baekjoon_Online_Judge/DFS_BFS/1697_숨바꼭질.py
@@ -1,3 +1,27 @@
 # https://www.acmicpc.net/problem/1697
 
 ############################################################
+
+def bfs(s, e):
+    q = []
+    v = [0 for _ in range(100002)]
+
+    q.append(s)
+    v[s] = 1
+
+    while q:
+        c = q.pop(0)
+        if c == e:
+            return v[c]-1
+        # 3방향, 범위내, 미방문, (조건)
+        for n in (c-1, c+1, 2*c):
+            if 0<=n<=100000 and not v[n]:
+                q.append(n)
+                v[n] = v[c] + 1
+
+    return -1
+
+n, k = map(int, input().split())
+
+ans = bfs(n, k)
+print(ans)
